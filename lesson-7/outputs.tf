da167ad6f60a1f1d8f4916f682bfcf2a01e9dd1c
Serhii Mishovych <serhiimishovych@gmail.com>
2025-11-22 15:58:58 +0100

    Added Helm

--- lesson-7/outputs.tf
+++ lesson-7/outputs.tf
@@ -1,3 +1,11 @@
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
 output "ecr_repo_url" {
   value = module.ecr.repository_url
 }
--- lesson-7/variables.tf
+++ lesson-7/variables.tf
@@ -9,3 +9,13 @@ variable "dynamodb_table_name" {
 variable "aws_region" {
   type = string
 }
+
+variable "cluster_name" {
+  type    = string
+  default = "lesson-7-eks"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.30"   
+}
